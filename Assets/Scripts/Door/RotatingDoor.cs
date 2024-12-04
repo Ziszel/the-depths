ceeffe829f174083c2543dd524f85a16f69d308c
Ziszle <the8thnoteofficial@gmail.com>
2024-12-04 15:39:56 +0200

    Door now rotates using Quaternion.RotateTowards to offer more control over the speed of the rotation. We may need to support a Slerp rotation later but this will need to be a separate implementation. Pivot added to allow for rotations of the DoorMesh from outside of the origin (this may not be needed once model is added), and prefab created for easy use.

--- Assets/Scripts/Door/RotatingDoor.cs
+++ Assets/Scripts/Door/RotatingDoor.cs
@@ -1,31 +1,35 @@
 using System.Collections;
-using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
 
 public class RotatingDoor : DoorBase, ISwitchable
 {
     // Rotate by this amount of degrees over movementDuration
     [SerializeField] private Vector3 rotateByDegrees = new Vector3(0.0f, 0.0f, 0.0f);
+    [SerializeField] private float rotationSpeed;
+
+    private Quaternion initialRotation;
+    private Quaternion endRotation;
     
     private void Start()
     {
+        initialRotation = transform.rotation;
+        endRotation = Quaternion.Inverse(initialRotation) * Quaternion.Euler(rotateByDegrees);
         _doorAudio = GetComponent<DoorAudio>();
     }
-    
+
     protected override IEnumerator OpenDoor()
     {
-        float timeElapsed = 0.0f;
-        var endRotation = transform.rotation * Quaternion.Euler(rotateByDegrees);
         
-        while (timeElapsed < movementDuration)
+        while (Quaternion.Angle(initialRotation, endRotation) > 0.01f)
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, endRotation, timeElapsed / movementDuration);
-            timeElapsed += Time.deltaTime;
+            // returns a quaternion rotated towards endRotation by the step value
+            // Updates initialRotation since the next frame will rotate from that point
+            initialRotation = Quaternion.RotateTowards(initialRotation, endRotation, rotationSpeed * Time.deltaTime);
+            transform.rotation = initialRotation;
             yield return null;
         }
-        
+
         transform.rotation = endRotation;
-        Debug.Log("Door is opened");
     }
     
     public void Toggle()
