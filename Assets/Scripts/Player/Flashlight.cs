843e3ac22e0f620c94052226d750c367e9f3df9b
Ziszle <the8thnoteofficial@gmail.com>
2025-01-08 12:37:45 +0200

    I wasn't a fan of the hooking up the flashlight to the MainCamera approach as delays became impossible. I have decided to re-create the Player prefab with an empty gameobject as a parent and two children beneath: One for the PlayerController and all none visible components, and for the second, the flashlight. After hooking up the flashlight to follow Camera.main (with a stored reference for performance considerations) and the use of quaternion slerp, the flashlight can now be delayed when looking towards where the camera is facing. There is a small rotation issue remaining afterwhich I will attempt to put a cookie onto the flashlight (this may not be possible due to URP restrictions in previous version of unity though there is a checkbox on the light component itself that I can not click, further investigation required.)

--- Assets/Scripts/Player/Flashlight.cs
+++ Assets/Scripts/Player/Flashlight.cs
@@ -3,27 +3,43 @@ using UnityEngine;
 
 public class Flashlight : MonoBehaviour
 {
+    public PlayerController playerController;
+    
     [Header("light Properties")]
     [SerializeField] public Light lightSource;
     [SerializeField] private float flashlightIntensity;
     [SerializeField] private float flashlightRange;
     [SerializeField] private float windupTime; // in seconds
+    [SerializeField] private float rotationSpeed;
     
     // Components
     private FlashlightAudio _flashlightAudio;
-    public PlayerController _playerController;
+    private Vector3 _offset;
+    private Camera _cameraToFollow;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
+        // Setup the light source
         lightSource = GetComponentInChildren<Light>();
         _flashlightAudio = GetComponentInChildren<FlashlightAudio>();
         lightSource.intensity = flashlightIntensity;
         lightSource.range = flashlightRange;
         
+        // Camera setup
+        _cameraToFollow = Camera.main;
+        _offset = transform.position - _cameraToFollow.transform.position;
+        
         // Hook up external events
-        _playerController.OnFlashlightActivated += ActivateFlashlight;
-        _playerController.OnFlashlightDeActivated += DeactivateFlashlight;
+        playerController.OnFlashlightActivated += ActivateFlashlight;
+        playerController.OnFlashlightDeActivated += DeactivateFlashlight;
+    }
+
+    private void Update()
+    {
+        transform.position = _cameraToFollow.transform.position + _offset;
+        transform.rotation = Quaternion.Slerp(transform.rotation, _cameraToFollow.transform.rotation, 
+            Time.deltaTime * rotationSpeed);
     }
 
     private IEnumerator LightActivationRoutine()
