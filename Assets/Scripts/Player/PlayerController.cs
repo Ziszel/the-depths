775c8e591811037098b6bcf5ecb179ebec0f0117
Ziszle <the8thnoteofficial@gmail.com>
2025-02-16 18:46:28 +0200

    Fixed player sprint bug and footstep rate bug. I have taken the opportunity to convert sprinting to events rather than a check in update. The new OnSprint function handles this. In addition, there is now an event on Stamina that is invoked when stamina runs out and the player is subscribed to this to ensure that sprinting stops even if the player is holding down the button still.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -28,7 +28,7 @@ public class PlayerController : MonoBehaviour
     [Header("Camera targets")]
     [SerializeField] private GameObject crouch; // update OnPlayerKill and look to remove this
 
-    [Header("Footstep play rates")] 
+    [Header("Footstep play rates (Audio)")] 
     [SerializeField] private float walkingRate = 1.0f;
     [SerializeField] private float sprintingRate = 0.5f;
     
@@ -78,7 +78,9 @@ public class PlayerController : MonoBehaviour
         _fpsCamera = GameObject.Find("FPSCamera").GetComponent<CinemachineCamera>();
         _interactable = null;
         _timeUntilFootstep = 0.0f; // stops it playing immediately or causing error
+        _currentFootstepRate = walkingRate;
         _isPlayerWalking = false;
+        _isCrouching = false;
         
         if (isDebug)
         {
@@ -92,11 +94,13 @@ public class PlayerController : MonoBehaviour
         {
             _monster.OnPlayerWithinDamageDistance += DamagePlayer;
         }
+
+        _stamina.OnStaminaReachedZero += SetPlayerSprintToWalk;
     }
 
     private void Update()
     {
-        // Handle timers
+        // Handle timers for footsteps
         if (_isPlayerWalking && !_isCrouching)
         {
             _timeUntilFootstep -= Time.deltaTime;
@@ -112,6 +116,7 @@ public class PlayerController : MonoBehaviour
             _timeUntilFootstep = _currentFootstepRate;
         }
 
+        // Handles if the player pressed a walk button multiple times per frame
         if (_inputActions.Player.Move.WasPerformedThisFrame())
         {
             if (_floorCollider.IsOnGround() && !_isCrouching)
@@ -137,7 +142,6 @@ public class PlayerController : MonoBehaviour
             }
         }
         
-        SetMovementValues();
         // Rotate the player to face the direction the camera is looking at
         transform.rotation = Quaternion.AngleAxis(_mainCamera.transform.eulerAngles.y, Vector3.up);
         
@@ -220,6 +224,31 @@ public class PlayerController : MonoBehaviour
         {
             _cameraManager.SwitchCamera(_cameraManager.fpsCamera);
         }
+    }
+
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            if (_floorCollider.IsOnGround() && !_isCrouching && 
+                _stamina.GetCurrentStamina() > 0.0f
+                && !_stamina.GetRegeneratingFromZero())
+            {
+                _currentFootstepRate = sprintingRate;
+                maxMovementVelocity = maxSprintVelocity;
+            }
+        } ;
+
+        if (context.canceled)
+        {
+            SetPlayerSprintToWalk();
+        }
+    }
+
+    private void SetPlayerSprintToWalk()
+    {
+        _currentFootstepRate = walkingRate;
+        maxMovementVelocity = maxWalkVelocity;
     }
 
     public void OnCrouch(InputAction.CallbackContext context)
@@ -311,19 +340,6 @@ public class PlayerController : MonoBehaviour
         _fpsCamera.Lens.Dutch = 90.0f;
         _fpsCamera.Target.TrackingTarget = crouch.transform;
         OnPlayerDeath?.Invoke();
-    }
-
-    private void SetMovementValues()
-    {
-        // Check if sprinting (TODO: Convert over to event)
-        if (Mathf.Approximately(_inputActions.Player.Sprint.ReadValue<float>(), 1.0f) 
-                 && _stamina.GetCurrentStamina() > 0.0f 
-                 && !_stamina.GetRegeneratingFromZero())
-        {
-            maxMovementVelocity = maxSprintVelocity;
-            _currentFootstepRate = sprintingRate;
-            _isCrouching = false;
-        }
     }
 
     private void Move()
