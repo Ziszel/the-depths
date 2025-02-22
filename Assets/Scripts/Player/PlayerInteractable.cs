fee2c51320d5fb833fb6ec35dbbcf1e9a19134d6
Ziszle <the8thnoteofficial@gmail.com>
2025-02-22 11:26:23 +0200

    Setup raycast and a new layer (InteractableMesh) that is set on the mesh of each Pickup item as well as adding a mesh collider to those items. This allows me to activate the item check and handle the UI check via raycast separately which is useful for enabling and disabling the UI via raycast within the enabled Interactable space. The raycast is not yet setup based on where the camera is looking meaning high-up / low-down items can not be looked for. This is the next step after which I can add back in the ability to pickup items with a button press and then add polish (highlighting, spatial text, etc...)

--- Assets/Scripts/Player/PlayerInteractable.cs
+++ Assets/Scripts/Player/PlayerInteractable.cs
@@ -1,24 +1,41 @@
+using System;
 using UnityEngine;
 
 public class PlayerInteractable : MonoBehaviour
 {
     [SerializeField] private PlayerHUDUI pcHud;
     private Pickup _pickupInRange;
+    
+    private LayerMask _layerMask;
 
-    private void Update()
+    private void Start()
     {
-        Debug.Log("I'm active");
+        _layerMask = LayerMask.GetMask("InteractableMesh");
+    }
+
+    private void FixedUpdate()
+    {
         if (DetectItem())
         {
             pcHud.SetActiveInteractable(_pickupInRange.GetInteractableSprite());
         }
+        else
+        {
+            pcHud.DisableActiveInteractable();
+        }
+        
     }
 
     private bool DetectItem()
     {
-        // Each frame see if the center of the camera is actually pointing at an item
-        // Debug.DrawRay(transform.position, transform.forward, Color.red);
-        return true;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(transform.position, fwd, Color.red);
+        if (Physics.Raycast(transform.position, fwd, 3, _layerMask))
+        {
+            return true;
+        }
+
+        return false;
     }
     
     public void SetActivePickup(Pickup pickup)
