61c270f803a7e61b3018b24ee1c224978a6593d5
ziszel <the8thnoteofficial@gmail.com>
2025-04-07 10:55:57 +0300

    Updated PlayerHUDUI to work with a generic descriptive text script. Now, any object in the game can easily be updated to display text to the playerHUD. I have added information for the doors (locked and unlocked) for the functional play test.

---
+++ Assets/Scripts/InteractableEvents/DisplayDescriptiveText.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+public class DisplayDescriptiveText : MonoBehaviour
+{
+    public static event Action<string> OnTextUpdate;
+    
+    [SerializeField] private string descriptionText;
+
+    public void UpdateDescriptionText()
+    {
+        OnTextUpdate?.Invoke(descriptionText);
+    }
+
+    public void UpdateDescriptionText(string text)
+    {
+        descriptionText = text;
+        OnTextUpdate?.Invoke(descriptionText);
+    }
+}
