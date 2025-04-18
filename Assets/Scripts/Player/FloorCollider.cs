69399d82a9ec30f6a801b32eda181f0eead029ac
ziszel <the8thnoteofficial@gmail.com>
2025-04-18 12:02:53 +0300

    Reworked PlayerAudio to utilise arrays and a random footstep sound instead of the left and right setup used previously. Metal (SolidSteel) sounds have now been added to the project. PlayerAudio now plays concrete and metal sounds correctly based on the specific FloorTag and utilises random sounds from the array passed in.

--- Assets/Scripts/Player/FloorCollider.cs
+++ Assets/Scripts/Player/FloorCollider.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 
 public class FloorCollider : MonoBehaviour
 {
+    [Header("External Components")]
     public PlayerController player;
+    public PlayerAudio playerAudio;
+    
     [SerializeField] private AudioClip landingGroundClip;
     [SerializeField] private float longFlightTimeThreshold;
     
-    private bool _onGround;
-    
+    // Components
     private GlobalSFXPlayer _globalSfxPlayer;
+    
     private bool _isHighFall;
+    private bool _onGround;
 
     private void Start()
     {
@@ -31,7 +35,7 @@ public class FloorCollider : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Floor"))
+        if (other.tag.Contains("Floor"))
         {
             if (_isHighFall)
             {
@@ -40,12 +44,31 @@ public class FloorCollider : MonoBehaviour
                 player.SetPlayerState(playerActionState.Standing);
             }
             _isHighFall = false;
+            
+            // Update footstep sounds
+            if (other.tag.Contains("Cement"))
+            {
+                Debug.Log("Walking on cement");
+                playerAudio.SetFootstepsToCement();
+            }
+            
+            if (other.tag.Contains("Wood"))
+            {
+                Debug.Log("Walking on wood");
+                playerAudio.SetFootstepsToWood();
+            }
+            
+            if (other.tag.Contains("SolidSteel"))
+            {
+                Debug.Log("Walking on solid steel");
+                playerAudio.SetFootstepsToSolidSteel();
+            }
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Floor"))
+        if (other.tag.Contains("Floor"))
         {
             if (!_onGround)
             {
@@ -57,7 +80,7 @@ public class FloorCollider : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Floor"))
+        if (other.tag.Contains("Floor"))
         {
             StartCoroutine(LongFlightTime());
             player.GetRigidBody().linearDamping = 1;
